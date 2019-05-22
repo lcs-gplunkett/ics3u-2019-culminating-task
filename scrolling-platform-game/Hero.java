6277e7e01b2377185f0d5bdab50b467801a6ea76
Gavin Plunkett <gplunkett@lcs.on.ca>
2019-05-22 11:53:27 -0400

    working with images

--- scrolling-platform-game/Hero.java
+++ scrolling-platform-game/Hero.java
@@ -69,7 +69,7 @@ public class Hero extends Actor
         horizontalDirection = FACING_RIGHT;
 
         // Set image
-        setImage("hero-jump-down-right.png");
+        setImage("space-right-0.png");
 
         // Initialize the 'walking' arrays
         walkingRightImages = new GreenfootImage[COUNT_OF_WALKING_IMAGES];
@@ -78,7 +78,7 @@ public class Hero extends Actor
         // Load walking images from disk
         for (int i = 0; i < walkingRightImages.length; i++)
         {
-            walkingRightImages[i] = new GreenfootImage("hero-walk-right-" + i + ".png");
+            walkingRightImages[i] = new GreenfootImage("space-right-" + i + ".png");
 
             // Create left-facing images by mirroring horizontally
             walkingLeftImages[i] = new GreenfootImage(walkingRightImages[i]);
@@ -147,11 +147,11 @@ public class Hero extends Actor
             // Set image
             if (horizontalDirection == FACING_RIGHT && Greenfoot.isKeyDown("right") == false)
             {
-                setImage("hero-right.png");
+                setImage("space-right-0.png");
             }
             else if (horizontalDirection == FACING_LEFT && Greenfoot.isKeyDown("left") == false)
             {
-                setImage("hero-left.png");
+                setImage("space-left-0.png");
             }
 
             // Get a reference to any object that's created from a subclass of Platform,
@@ -215,11 +215,11 @@ public class Hero extends Actor
         // Set image
         if (horizontalDirection == FACING_RIGHT)
         {
-            setImage("hero-jump-up-right.png");
+            setImage("space-right-1.png");
         }
         else
         {
-            setImage("hero-jump-up-left.png");
+            setImage("space-left-1.png");
         }
 
         // Change the vertical speed to the power of the jump
@@ -242,11 +242,11 @@ public class Hero extends Actor
             // Set image
             if (horizontalDirection == FACING_RIGHT)
             {
-                setImage("hero-jump-down-right.png");
+                setImage("space-right-1.png");
             }
             else
             {
-                setImage("hero-jump-down-left.png");
+                setImage("space-left-1.png");
             }
         }
 
@@ -307,11 +307,11 @@ public class Hero extends Actor
             // Set appropriate jumping image
             if (verticalDirection == JUMPING_UP)
             {
-                setImage("hero-jump-up-right.png");
+                setImage("space-right-1.png");
             }
             else
             {
-                setImage("hero-jump-down-right.png");
+                setImage("space-right-0.png");
             }
         }
 
@@ -419,11 +419,11 @@ public class Hero extends Actor
             // Set appropriate jumping image
             if (verticalDirection == JUMPING_UP)
             {
-                setImage("hero-jump-up-left.png");
+                setImage("space-left-1.png");
             }
             else
             {
-                setImage("hero-jump-down-left.png");
+                setImage("space-left-0.png");
             }
         }
 
